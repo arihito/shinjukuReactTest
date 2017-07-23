b97efd7e2ed450fc7c1af219aeaea093754df0d4
arihito <arihito.m@gmail.com>
2017-07-23 15:55:03 +0900

    failed to compiled error index.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+
+import { Route, Link, withRouter } from 'react-router-dom'
+
 import NumberCmp from './NumberCmp';
 import FormCmp from './FormCmp';
 import FetchCmp from './FetchCmp';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.history.push('/fetch')
+  }
+
   render() {
     return (
-        <div>
-          <NumberCmp number="1"/>
-          <FormCmp />
-          <FetchCmp />
-        </div>
+      <div>
+        <ol>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/number">Number</Link></li>
+          <li><Link to="/form">Form</Link></li>
+          <li><button onClick={this.handleClick}>Fetch</button></li>
+        </ol>
+        <Route path="/number" component={NumberCmp} number={2} />
+        <Route path="/form" component={FormCmp} />
+        <Route path="/fetch" component={FetchCmp} />
+      </div>
     )
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom' // add
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// add Router element
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+document.getElementById('root'));
 registerServiceWorker();
